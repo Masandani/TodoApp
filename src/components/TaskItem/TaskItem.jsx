d07ac441d4c97b5fa9b0afadf2bae4a4ddbac062
Masandani <masandani3@gmail.com>
2021-08-30 20:35:58 +0430

    commit on main

--- src/components/TaskItem/TaskItem.jsx
+++ src/components/TaskItem/TaskItem.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FaTrashAlt } from 'react-icons/fa';
+// import { FaTrashAlt } from 'react-icons/fa';
 import './TaskItem.css'
 
 const TaskItem = ({ task, deleteTask, handleChangeStatus }) => (
@@ -7,7 +7,7 @@ const TaskItem = ({ task, deleteTask, handleChangeStatus }) => (
         <li className="TaskItem">
             <input onChange={() => handleChangeStatus(task.id)} type="checkbox" checked={task.status} />
             <h2>{task.title}</h2>
-            <button onClick={() => deleteTask(task.id)}><FaTrashAlt /></button>
+            <button onClick={() => deleteTask(task.id)}>X</button>
         </li>
     </div>
 );
